e7b8b45383965835c2db8f0a89d1fc6e41bdb80c
Anoxia <909483306@qq.com>
2020-09-19 18:55:06 +0800

    第一次进行数据的提交

---
+++ TestDeom01/src/com/fan/test01/TestMain.java
@@ -0,0 +1,14 @@
+package com.fan.test01;
+
+/**
+ * @Auther: fzw
+ * @Date: 2020/9/19 - 09 - 19 - 18:17
+ * @Description: com.fan.test01
+ * @version: 1.0
+ */
+public class TestMain {
+    //这是main方法,程序的入口:
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
